849885ac98f09f844c3f28a3ecd9b84a5355662f
tantyu2021 <tantyu2021@gmail.com>
2020-02-25 11:59:38 -0800

    add correct. in html, js02

--- js/02-fortune-teller.js
+++ js/02-fortune-teller.js
@@ -1,8 +1,5 @@
 // a function named tellFortune()- • takes 4 arguments: number of children, partner's name, geographic location, job title. - •	Outputs your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids." - •	Call that function 3 times with 3 different values for the arguments.
-let children;
-let parentName;
-let location;
-let jobTitle;
+
 function tellFortune (children, parentName, location, jobTitle) {
 
 }
