358a1ee7ac8c2b9457358c367a5d697c165a1c2b
tantyu2021 <tantyu2021@gmail.com>
2020-02-25 11:54:01 -0800

    add in js01,  js02

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -1,40 +1,41 @@
-//STEP 1 - 1.	Write a function called halfNumber() that accepts one argument (a number), divide it by 2, and return the result. It should log a string like "Half of 5 is 2.5.".
-function halfNumber(number) {
-   
+//STEP 1.	Write a function called halfNumber() that accepts one argument (a number), divide it by 2, and return the result. It should log a string like "Half of 5 is 2.5.".
+console.log("STEP 1");
+
+function halfNumber(num) {
+  var half = num /2;
+  console.log("Half of " + num + " is " + half);
+  return half;
 }
-window.console.log("Half of 5 is 2.5.");
+halfNumber(5)
 
-// ADVANCED AGRUMENT EXAMPLE
-/*
-function calculate(numbers) {
-    let base = Number(numbers);
-    let i;
-    let (i = 1; i < arguments.lengs; i += 1) {
-base += Number (arguments[i]);
-   
-document.write("The calculation remainit " + base);
+//STEP 2.	Write a function called squareNumber() that accepts one argument (a number), square that number, and return the result. It should also log a string like "The result of squaring the number 3 is 9."
+console.log("STEP 2");
+        
+function squareNumber(num) {
+var squareNamber = num * num;
+console.log('The result of squaring the number ' + num + ' is ' + squareNamber);
+return squareNamber;
 }
-calculate(10, 10, 200);
-*/
+squareNumber(3);
 
+//STEP 3.	Write a function called percentOf() that accepts two numbers, figure out what percent the first number represents of the second number, and return the result. It should log a string like "2 is 50% of 4."
+console.log("STEP 3");
 
-
-
-//  window.alert("Half of 5 is 2.5.");
-
-//STEP 2 - 2.	Write a function called squareNumber() that accepts one argument (a number), square that number, and return the result. It should also log a string like "The result of squaring the number 3 is 9."
-function squareNumber(number) {
-    
+function percentOf(num1, num2) {
+  var percent = (num1 / num2) * 100;
+  console.log(num1 + " is " + percent + "% of " + num2);
+  return percent;
 }
-window.console.log("The result of squaring the number 3 is 9.");
-//STEP 3 - 3.	Write a function called percentOf() that accepts two numbers, figure out what percent the first number represents of the second number, and return the result. It should log a string like "2 is 50% of 4."
-function percentOf() {
-    
-}
-window.console.log("2 is 50% of 4.");
-//STEP 4 - 4.	Write a function called findModulus() that accepts two numbers. Within the function write a statement that returns the modulus of the first and second parameters. It should log a string like "2 is the modulus of 4 and 10."
-function findModulus() {
-    
-}
-window.console.log("2 is the modulus of 4 and 10.");
+percentOf(2,4)
+
+//STEP 4.	Write a function called findModulus() that accepts two numbers. Within the function write a statement that returns the modulus of the first and second parameters. It should log a string like "2 is the modulus of 4 and 10."
+console.log("STEP 4")
+        
+function findModulus(num1, num2) {
+  var modulus = num1 /2;
+  console.log(modulus + " is the modulus of " + num1 + " and " + num2);
+  return modulus;
+}        
+findModulus(4, 10)  
+
 //STEP 5 - 5.	Create a JavaScript function that accepts a certain amount of numbers as parameters. Those numbers should be collected using a prompt and the numbers should be delimited by commas. Once the values are collected, find the sum of all of the numbers combined. You will need to use a function, loop, arguments object, and several type conversion global functions to accomplish this task.
--- js/02-fortune-teller.js
+++ js/02-fortune-teller.js
@@ -7,3 +7,12 @@ function tellFortune (children, parentName, location, jobTitle) {
 
 }
 window.console.log("You will be a X in Y, and married to Z with N kids.");
+          
+ 
+function tellFortune (children, partner, location, jobTitle) {
+    var future = 'You will be a ' + jobTitle + ' in ' + location + ', and married to ' + partner + ' ' + ' with ' + children + ' kids.';
+console.log(future);  
+  }      
+    tellFortune ('1', 'Misha','Ocean Side ', 'teacher')    
+    tellFortune ('2', 'Odo','Barbados ', 'salor')  
+    tellFortune ('4', 'Amaly', 'Maui ', 'housekiper')  
