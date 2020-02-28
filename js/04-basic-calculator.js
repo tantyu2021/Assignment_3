a17a5a4652048c1ebf20f50795ac2f5cd5cdbc0e
tantyu2021 <tantyu2021@gmail.com>
2020-02-27 23:25:04 -0800

    any correct js04

--- js/04-basic-calculator.js
+++ js/04-basic-calculator.js
@@ -7,8 +7,8 @@
 // 6.	Within the calculate() function use a switch statement to evaluate the operation parameter being passed in. Depending on what it is, perform that operation, and display the result within an alert box.
 // 7.	Figure out how to make it so that if the user doesn’t enter either add, subtract, multiply, or divide, they are displayed an alert message telling them that. Then you’ll have to rerun the application so that the application asks the user for the numbers and operation once more.
 /* with a SWITCH statement */
-var num1 = parseFloat(window.prompt("Enter the number"));
-var num2 = parseFloat(window.prompt("Enter the number."));
+var num1 = parseFloat(window.prompt("Please, Enter the number"));
+var num2 = parseFloat(window.prompt("Please, Enter the next number."));
 var choice = window.prompt('Make choice of math operation: (add, subtract, multiply, divide)'); 
    
 function calculate(num1, num2, choice) {  
